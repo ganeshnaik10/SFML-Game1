991a3b6f0ed47902f8929d7fb31678b82b0e77f3
ganeshnaik10 <ganeshnaik6762@gmail.com>
2025-09-26 02:01:20 +0530

    game window creation

---
+++ project.cpp
@@ -0,0 +1,16 @@
+#include <SFML/Graphics.hpp>
+#include <optional>
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "My window");
+
+    while (window.isOpen()) {
+        while (const std::optional event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear();   
+        window.display(); 
+    }
+}
